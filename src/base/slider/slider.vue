f4578c07524ec7889114793dd81f21f772f0fb05
qinjunshan <13782533791@163.com>
2018-07-06 00:25:11 +0800

    添加轮播，滚动

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,15 +1,21 @@
 <template>
-  <div class="slider">
-    <div class="slider-group">
-      <!--插槽-->
-      <slot></slot>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot>
+      </slot>
     </div>
-    <div class="dots"></div>
+    <div class="dots">
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots"></span>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import {addClass} from '@/common/js/dom'
+  import BScroll from 'better-scroll'
+
   export default {
+    name: 'slider',
     props: {
       loop: {
         type: Boolean,
@@ -17,17 +23,94 @@
       },
       autoPlay: {
         type: Boolean,
-        default:true
+        default: true
       },
       interval: {
         type: Number,
         default: 4000
       }
     },
-    data(){
+    data() {
       return {
         dots: [],
         currentPageIndex: 0
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._setSliderWidth()
+        this._initDots()
+        this._initSlider()
+
+        if (this.autoPlay) {
+          this._play()
+        }
+      }, 20)
+
+      window.addEventListener('resize', () => {
+        if (!this.slider) {
+          return
+        }
+        this._setSliderWidth(true)
+        this.slider.refresh()
+      })
+    },
+    destroyed() {
+      clearTimeout(this.timer)
+    },
+    methods: {
+      _setSliderWidth(isResize) {
+        this.children = this.$refs.sliderGroup.children
+
+        let width = 0
+        let sliderWidth = this.$refs.slider.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          addClass(child, 'slider-item')
+
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+        }
+        if (this.loop && !isResize) {
+          width += 2 * sliderWidth
+        }
+        this.$refs.sliderGroup.style.width = width + 'px'
+      },
+      _initSlider() {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: true,
+          snapLoop: this.loop,
+          snapThreshold: 0.3,
+          snapSpeed: 400
+        })
+
+        this.slider.on('scrollEnd', () => {
+          let pageIndex = this.slider.getCurrentPage().pageX
+          if (this.loop) {
+            pageIndex -= 1
+          }
+          this.currentPageIndex = pageIndex
+
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+            this._play()
+          }
+        })
+      },
+      _initDots() {
+        this.dots = new Array(this.children.length)
+      },
+      _play() {
+        let pageIndex = this.currentPageIndex + 1
+        if (this.loop) {
+          pageIndex += 1
+        }
+        this.timer = setTimeout(() => {
+          this.slider.goToPage(pageIndex, 0, 400)
+        }, this.interval)
       }
     }
   }
@@ -60,7 +143,6 @@
       right: 0
       left: 0
       bottom: 12px
-      transform: translateZ(1px)
       text-align: center
       font-size: 0
       .dot
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,50 +1,84 @@
 <template>
-  <div class="recommend">
-    <div class="recommend-content">
-      <div class="slider-wrapper">
-        <slider>
-          <div v-for="item in recommends" :key="item.linkUrl">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl" alt="">
-            </a>
-          </div>
-        </slider>
+  <div class="recommend" ref="recommend">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60"  v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <ul>
-
-        </ul>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
       </div>
-    </div>
+    </scroll>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import {getRecommend} from "@/api/recommend"
-  import {ERR_OK} from "@/api/config"
   import Slider from '@/base/slider/slider'
+  import Loading from '@/base/loading/loading'
+  import Scroll from '@/base/scroll/scroll'
+  import {getRecommend, getDiscList} from '@/api/recommend'
+  import {ERR_OK} from '@/api/config'
 
   export default {
-    components: {
-      Slider
-    },
     data() {
       return {
-        recommends: []
+        recommends: [],
+        discList: []
       }
     },
     created() {
       this._getRecommend()
+
+      this._getDiscList()
     },
     methods: {
+      loadImage() {
+        if (!this.checkloaded) {
+          this.checkloaded = true
+          this.$refs.scroll.refresh()
+        }
+      },
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
           }
         })
+      },
+      _getDiscList() {
+        getDiscList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.discList = res.data.list
+          }
+        })
       }
+    },
+    components: {
+      Slider,
+      Loading,
+      Scroll
     }
   }
 </script>
@@ -63,15 +97,7 @@
       .slider-wrapper
         position: relative
         width: 100%
-        height: 0
-        padding-top: 40%
         overflow: hidden
-        .slider-content
-          position: absolute
-          top: 0
-          left: 0
-          width: 100%
-          height: 100%
       .recommend-list
         .list-title
           height: 65px
