7ce7f1b3c7df680a9853f985e4552b0fa1d7e5bd
qinjunshan <13782533791@163.com>
2018-07-07 23:32:05 +0800

    format

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60"  v-lazy="item.imgurl">
+                <img width="60" height="60" v-lazy="item.imgurl">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
