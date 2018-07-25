4c6620ddfbab018adc189a53b3c966af4212f9b8
qinjunshan <13782533791@163.com>
2018-07-25 23:35:29 +0800

    format

--- src/base/progress-circle/progress-circle.vue
+++ src/base/progress-circle/progress-circle.vue
@@ -2,7 +2,8 @@
   <div class="progress-circle">
     <svg :width="radius" :height="radius" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
       <circle class="progress-background" r="50" cx="50" cy="50" fill="transparent"/>
-      <circle class="progress-bar" r="50" cx="50" cy="50" fill="transparent" :stroke-dasharray="dashArray" :stroke-dashoffset="dashOffset"/>
+      <circle class="progress-bar" r="50" cx="50" cy="50" fill="transparent" :stroke-dasharray="dashArray"
+              :stroke-dashoffset="dashOffset"/>
       <!--stroke-dashoffset: 0，全部包括-->
     </svg>
     <slot></slot>
