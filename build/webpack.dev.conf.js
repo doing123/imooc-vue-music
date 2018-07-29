5c8f22c199812f62b9dabef016b093ac3f611991
qinjunshan <13782533791@163.com>
2018-07-29 18:46:06 +0800

    搜索页面：suggest组件开发。搜索历史待完善

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -107,6 +107,21 @@ const devWebpackConfig = merge(baseWebpackConfig, {
           console.log(e)
         })
       })
+
+      app.get('/api/search', function (req, res) {
+        const url = 'https://c.y.qq.com/soso/fcgi-bin/search_for_qq_cp'
+        axios.get(url, {
+          headers: {
+            referer: 'https://c.y.qq.com/',
+            host: 'c.y.qq.com'
+          },
+          params: req.query
+        }).then((response) => {
+          res.json(response.data)
+        }).catch((e) => {
+          console.log(e)
+        })
+      })
     },
 
     clientLogLevel: 'warning',
