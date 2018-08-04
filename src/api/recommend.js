ead0ccb6690076f117dd1cbf1a2fb94537edcce4
qinjunshan <13782533791@163.com>
2018-08-04 23:49:03 +0800

    over

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -17,7 +17,7 @@ export function getRecommend() {
 }
 
 export function getDiscList() {
-  // 线上环境地址，同学们根据自己的需要配置修改 // todo: 设置？？？
+  // 线上环境地址，同学们根据自己的需要配置修改 // todo: 设置
   const url = debug ? '/api/getDiscList' : 'http://ustbhuangyi.com/music/api/getDiscList'
 
   const data = Object.assign({}, commonParams, {
